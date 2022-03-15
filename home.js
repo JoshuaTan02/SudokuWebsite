8a0e6a903bb76db51530f2c364260646cdff6179
JoshuaTan <JoshuaTan@users.noreply.github.com>
2022-03-15 20:52:38 +0800

    Updated Board
    I am able to update the board with a selected board

--- home.js
+++ home.js
@@ -12,4 +12,20 @@ function changeColor(object){
     object.style.backgroundColor = "red";
 }
 let boardNumber = Math.floor(Math.random() * boards.length);
-console.log(boards[boardNumber])
+let board = boards[boardNumber]
+
+function updateBoard(){
+    var boardElement = document.getElementsByClassName("board")[0];
+    var boxes = boardElement.getElementsByClassName("col-1")
+    console.log(boxes.length)
+
+    for (var i = 0; i< board.length; i++){
+        for (var j = 0; j< board[0].length; j++){
+                let index = i*9  + j
+                boxes[index].textContent = board[i][j]
+            }   
+    }
+
+}
+
+updateBoard()
